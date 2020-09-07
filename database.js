794eb882b571271de9adefe9be836c9fcafd0c26
Sanyam Kalra <samkalra002@gmail.com>
2020-09-07 12:12:22 +0530

    Final

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = 'mongodb+srv://sanyam:root@cluster0-wlyxq.mongodb.net/test?retryWrites=true&w=majority'
-
+const connection = "mongodb+srv://sanyam:<password>@cluster0.wlyxq.mongodb.net/<databaseName>?retryWrites=true&w=majority"
 mongoose.connect(connection, { useNewUrlParser: true })
     .then(() => console.log("Database connected successfully"))
     .catch(err => console.log(err))
